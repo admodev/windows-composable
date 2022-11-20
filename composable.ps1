aafc234ed9edcb2310d8a5db9ca08306494212a4
admodev <admodevcodes@outlook.com>
2022-11-20 12:53:55 -0300

    add compose file creation and function for checking if it exists

--- composable.ps1
+++ composable.ps1
@@ -1,3 +1,16 @@
+function Create-Compose-File {
+  Write-Host "Creating compose file..."
+
+  New-Item .\docker-compose.yml
+
+  Write-Host "Done."
+}
+
+# Check if compose file exists
+function Not-Exist { -not (Test-Path $args) }
+Set-Alias !exist Not-Exist -Option "Constant, AllScope"
+Set-Alias exist Test-Path -Option "Constant, AllScope"
+
 function Get-Compose-Services {
   $ServiceName = Read-Host -Prompt "Please, name your service: "
 
@@ -5,6 +18,14 @@ function Get-Compose-Services {
 }
 
 Write-Host "Welcome to composable!"
-Write-Host "Please, detail the services and images you want to compose for build"
+Write-Host "Please, detail the services and images you want to compose for build when prompted."
+
+$FileExists = [System.IO.File]::Exists(".\docker-compose.y*ml")
+
+if (not-exist ".\docker-compose.y*ml") {
+  Create-Compose-File
+} else {
+  Write-Host "Compose file already exists, skipping..."
+}
 
 Get-Compose-Services
